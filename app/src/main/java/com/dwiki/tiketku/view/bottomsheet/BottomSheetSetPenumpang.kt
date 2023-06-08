a60b8234bd38c4648a6abca9ccb1fd3c5076a614
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2023-06-08 12:33:39 +0700

    [update] Bottom sheet penumpang

--- app/src/main/java/com/dwiki/tiketku/view/bottomsheet/BottomSheetSetPenumpang.kt
+++ app/src/main/java/com/dwiki/tiketku/view/bottomsheet/BottomSheetSetPenumpang.kt
@@ -1,31 +1,35 @@
 package com.dwiki.tiketku.view.bottomsheet
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dwiki.tiketku.R
+import com.dwiki.tiketku.databinding.FragmentBottomSheetSetPenumpangBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class BottomSheetKelasFragment : BottomSheetDialogFragment() {
+class BottomSheetSetPenumpang : BottomSheetDialogFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setStyle(STYLE_NORMAL, R.style.CustomBottomSheetDialog)
-    }
-
+    private lateinit var binding:FragmentBottomSheetSetPenumpangBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bottom_sheet_kelas, container, false)
+        binding = FragmentBottomSheetSetPenumpangBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnClose.setOnClickListener {
+            dismiss()
+        }
     }
+
 
 }
